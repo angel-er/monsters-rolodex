0d0bc6a9f1fe9fba2c75bcdc00ef38c7056a8975
root <angele.educar@gmail.com>
2019-10-15 11:02:43 -0300

    Updates

---
+++ precache-manifest.fc3cfa08e310308765479f77c3aef4dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ae2b4e23c0937dfa26ee43ef7d7bc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11ee85c81dce721211b2",
+    "url": "/static/css/main.f1ec41f3.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "11ee85c81dce721211b2",
+    "url": "/static/js/main.ef3ec298.chunk.js"
+  },
+  {
+    "revision": "1584f9862dc88a05382d",
+    "url": "/static/js/runtime-main.e32a2b7a.js"
+  }
+]);
